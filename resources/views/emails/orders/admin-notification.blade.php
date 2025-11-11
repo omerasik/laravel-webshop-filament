70349218c441190605221d021e43bf75add2a975
omerasik <omerasik60@gmail.com>
2025-11-11 19:48:47 +0100

    add order mails

---
+++ resources/views/emails/orders/admin-notification.blade.php
@@ -0,0 +1,12 @@
+﻿<p>Nieuwe bestelling binnen:</p>
+
+<p><strong>Ordernummer:</strong> {{ $order->id }}</p>
+<p><strong>Klant:</strong> {{ $order->user->email }}</p>
+
+<ul>
+    @foreach ($order->items as $item)
+        <li>{{ $item->product->name }} x {{ $item->quantity }}</li>
+    @endforeach
+</ul>
+
+<p>Totaal bedrag: &euro; {{ number_format($order->price, 2, ',', '.') }}</p>
---
+++ resources/views/emails/orders/customer-confirmation.blade.php
@@ -0,0 +1,15 @@
+﻿<p>Beste {{ $order->user->name }},</p>
+
+<p>Bedankt voor je bestelling bij Webshop Omerasik. Hieronder vind je een overzicht van je order:</p>
+
+<ul>
+    @foreach ($order->items as $item)
+        <li>{{ $item->product->name }} x {{ $item->quantity }} - &euro; {{ number_format($item->price * $item->quantity, 2, ',', '.') }}</li>
+    @endforeach
+</ul>
+
+<p>Totaal: &euro; {{ number_format($order->price, 2, ',', '.') }}</p>
+
+<p>We houden je op de hoogte van de verzending.</p>
+
+<p>Met vriendelijke groeten,<br>Webshop Omerasik</p>
