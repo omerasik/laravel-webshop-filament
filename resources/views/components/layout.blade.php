362a171b458293b9cfc4200af83fa1fba5c03bd4
omerasik <omerasik60@gmail.com>
2025-11-11 10:19:01 +0100

    add favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -35,6 +35,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700" rel="stylesheet" />
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
 
         {{-- Structured data (Schema.org) --}}
         @if ($schemaJson)
