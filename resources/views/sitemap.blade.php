f118bc7e7efaa6401e3ef480122201239371764e
omerasik <omerasik60@gmail.com>
2025-11-11 07:55:38 +0100

    add sitemap blade  view

---
+++ resources/views/sitemap.blade.php
@@ -0,0 +1,18 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+{{-- Sitemap met vaste pagina's en recente producten --}}
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+@foreach ($pages as $page)
+    <url>
+        <loc>{{ $page }}</loc>
+    </url>
+@endforeach
+@foreach ($products as $product)
+    <url>
+        <loc>{{ route('products.show', $product->id) }}</loc>
+        @if ($product->updated_at)
+        <lastmod>{{ $product->updated_at->toDateString() }}</lastmod>
+        @endif
+    </url>
+@endforeach
+</urlset>
+
