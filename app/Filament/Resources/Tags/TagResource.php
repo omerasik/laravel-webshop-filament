9fd1c79def259c8692a612d02d6435051190b647
omerasik <omerasik60@gmail.com>
2025-11-11 01:43:11 +0100

    tag management

---
+++ app/Filament/Resources/Tags/TagResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources\Tags;
+
+use App\Filament\Resources\Tags\Pages\ManageTags;
+use App\Models\Tag;
+use BackedEnum;
+use UnitEnum;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class TagResource extends Resource
+{
+    protected static ?string $model = Tag::class;
+
+    protected static UnitEnum|string|null $navigationGroup = 'Catalogus';
+
+    protected static BackedEnum|string|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->label('Tagnaam')
+                    ->helperText('Gebruik korte, duidelijke labels.')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('slug')
+                    ->label('Slug')
+                    ->helperText('Wordt automatisch ingevuld op basis van de naam.')
+                    ->disabled()
+                    ->dehydrated(false),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Tag')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('slug')
+                    ->label('Slug')
+                    ->searchable(),
+                TextColumn::make('products_count')
+                    ->counts('products')
+                    ->label('Producten')
+                    ->sortable(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ManageTags::route('/'),
+        ];
+    }
+}
